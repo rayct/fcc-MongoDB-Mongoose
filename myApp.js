0acec451299b7f814c9f696f4032df6fae1bd351
rayct <rayct@icloud.com>
2022-06-06 00:14:58 +0100

    Added mongoose require

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
+
+const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-const mongoose = require('mongoose');
 
 let Person;
 
